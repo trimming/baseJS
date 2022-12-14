aeab4b4498f19652c77ff7cf3aa554a57ffd1f3b
trimming <lexgolikoff@gmail.com>
2022-12-14 17:09:10 +0300

    some update from app.js

--- lesson-8/1 текст вместо картинки/app.js
+++ lesson-8/1 текст вместо картинки/app.js
@@ -22,22 +22,22 @@ function handleClick(clickedButtonEvent) {
     const cardNode = clickedButtonEvent.parentNode;
     // 3. создайте литерал объекта со следующими свойствами:
     const card = {
-        wrap: , // здесь элемент с классом .product
-        img: , // здесь картинка внутри .product
-        productName: , // здесь .productName, который внутри .product
-        button: , // здесь button, который внутри .product
+        wrap: cardNode, // здесь элемент с классом .product
+        img: cardNode.childNodes[3], // здесь картинка внутри .product
+        productName: cardNode.childNodes[1], // здесь .productName, который внутри .product
+        button: cardNode.childNodes[5], // здесь button, который внутри .product
     };
 
     // 4. получаем текст на кнопке, которая внутри .product
+    let textButton = card.button.textContent;
 
-
-    //  if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
-    // 4.2 если да, то передаем объект card в функцию showMoreText
-    //    showMoreText(card);
-    //} else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
-    // 4.4 если да, то передаем объект card в функцию hideMoreText
-    //     hideMoreText(card);
-    //}
+    if (textButton === "Подробнее") { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
+        // 4.2 если да, то передаем объект card в функцию showMoreText
+        showMoreText(card);
+    } else if (textButton === "Отмена") { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
+        // 4.4 если да, то передаем объект card в функцию hideMoreText
+        hideMoreText(card);
+    }
 }
 
 /**
@@ -48,14 +48,14 @@ function handleClick(clickedButtonEvent) {
  * @param {HTMLDivElement} card.productName
  * @param {HTMLButtonElement} card.button
  */
- //   function hideMoreText(card) {
-        // 5. картинке внутри .product ставим стиль display: block
+function hideMoreText(card) {
+    // 5. картинке внутри .product ставим стиль display: block
+    card.img.style.display = 'block';
+    // 5.1 внутри .product находим элемент с классом .desc и удаляем его
 
-        // 5.1 внутри .product находим элемент с классом .desc и удаляем его
+    // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
 
-        // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-
-   // }
+}
 
 /**
  * Функция показывает текст с описанием товара.
@@ -65,13 +65,12 @@ function handleClick(clickedButtonEvent) {
  * @param {HTMLDivElement} card.productName
  * @param {HTMLButtonElement} card.button
  */
- //   function showMoreText(card) {
-        // 6. картинке внутри .product ставим display: none
-
-        // 6.1 сохраняем произвольный текст в переменную
-
-        // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-
-        // 6.3 внутри .product у кнопки текст ставим "Отмена"
-
-  // 
+function showMoreText(card) {
+    // 6. картинке внутри .product ставим display: none
+    card.img.style.display = 'none';
+    // 6.1 сохраняем произвольный текст в переменную
+    let textDesc = "Lorem ipsum der brukin filk";
+    // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1    
+    card.productName.insertAdjacentHTML('afterend', `<div class = desc>${textDesc}</div>`);
+    // 6.3 внутри .product у кнопки текст ставим "Отмена"
+}
